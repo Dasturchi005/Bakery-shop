98c60dfa6f7dd2e0cdd66367a189741c9da82bf8
Abduvali <156578585+Dasturchi005@users.noreply.github.com>
2025-09-17 08:50:30 +0500

    First commit

--- core/settings.py
+++ core/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=_a(v)q=r*6wx-qr+32s2e=x4di#q7h6i6dk)4m7ov@#sepsf4'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.17').split(',')
 
 
 # Application definition
